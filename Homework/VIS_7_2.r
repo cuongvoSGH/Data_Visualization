329656e4cb57686c20855bc8510ac54cb6d4f850
cuongvoSGH <cuongvo.polnd@gmail.com>
2023-12-24 17:36:08 +0100

    ggplot2

--- Homework/VIS_7_2.r
+++ Homework/VIS_7_2.r
@@ -5,5 +5,7 @@ library(ggplot2)
 
 # 1. Please analyse using visual technics #economics dataset available in package # ggplot2.
 # 2. Visualize charasteristics of this dataset.
-# 3. Create bublechart for 3 variables of this dataset.
+# 3. Create bublechart for 3 variables of this dataset. --> create subset
 
+data("economics")
+economics
--- Homework/VIS_8_2.r
+++ Homework/VIS_8_2.r
@@ -1,13 +1,15 @@
 # Exercise.
 
-Before doing this exercise please install package ggplot2movies
+# Before doing this exercise please install package ggplot2movies
 
 library(ggplot2movies)
 ?movies
-
+data("movies")
 
 #Pleasae analyse dataset movies available after installing package ggplot2movies
 # 1. Please visualize main features of this dataset using ggplot2 package
 # 2. Create chart with few panels characterising 3 most important fetaures of
 # this dataset.
+
+# analyze the budget columns, create new dataset with only non NA obs in movies dataset
 
